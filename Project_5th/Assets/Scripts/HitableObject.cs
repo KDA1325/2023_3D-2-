63094f92c1ec81177fca76406abbc67f87c275f3
KDA1325 <1325680q@gmail.com>
2023-10-22 03:49:22 +0900

    18 구현

--- Project_5th/Assets/Scripts/HitableObject.cs
+++ Project_5th/Assets/Scripts/HitableObject.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class HitableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private MeshRenderer meshRenderer;
+    private Color originColor;
+
+    private void Awake()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+        originColor = meshRenderer.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        Debug.Log($"{damage}의 피해를 입었습니다.");
+
+        StartCoroutine(nameof(OnHitAnimation));
+    }
+
+    private IEnumerator OnHitAnimation()
+    {
+        meshRenderer.material.color = Color.black;
+
+        yield return new WaitForSeconds(0.1f);
+
+        meshRenderer.material.color = originColor;
     }
 }
